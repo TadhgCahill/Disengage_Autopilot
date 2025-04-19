c6db51ff33fd3f6859b53c743abc06789d10a220
TadhgCahill <tadhg.cahill@gmail.com>
2025-04-18 18:05:21 -0700

    Add files via upload

---
+++ button2.gd
@@ -0,0 +1,10 @@
+extends Button
+
+func _ready():
+	var button = Button.new()
+	button.text = "Click me"
+	button.pressed.connect(_button_pressed)
+	add_child(button)
+
+func _button_pressed():
+	print("Hello world!")
